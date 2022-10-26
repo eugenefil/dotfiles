5553ffce2bb49df9570e8d63549c896064a3cb85
eugenefil <fil.eugene@gmail.com>
2022-10-26 23:44:43 +0200

    Loading .Xresources is moved from .xinitrc to openbox autostart.sh

--- .config/openbox/autostart.sh
+++ .config/openbox/autostart.sh
@@ -1,3 +1,7 @@
-xterm -e tmux new-session \; new-window cmus \; select-window -t:^ &
+start_xterm() {
+	[ -f ~/.Xresources ] && xrdb -l ~/.Xresources
+	xterm -e tmux new-session \; new-window cmus \; select-window -t:^
+}
+start_xterm &
 emacs &
 firefox &
