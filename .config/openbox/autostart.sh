5871f240995efc69fb86eaaf36c15c17ae5de40f
eugenefil <fil.eugene@gmail.com>
2022-10-27 20:16:08 +0200

    openbox/autostart.sh: if possible tmux attaches to existing session

--- .config/openbox/autostart.sh
+++ .config/openbox/autostart.sh
@@ -1,6 +1,10 @@
 start_xterm() {
 	[ -f ~/.Xresources ] && xrdb -l ~/.Xresources
-	xterm -e tmux new-session \; new-window cmus \; select-window -t:^
+	if pgrep tmux; then
+		xterm -e tmux attach
+	else
+		xterm -e tmux new-session \; new-window cmus \; select-window -t:^
+	fi
 }
 start_xterm &
 emacs &
