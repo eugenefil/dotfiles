28d8931c52c5553a10dbb69b0307a01ca51485f5
eugenefil <fil.eugene@gmail.com>
2022-10-27 21:43:02 +0200

    openbox/autostart.sh: forgot to exec xterm

--- .config/openbox/autostart.sh
+++ .config/openbox/autostart.sh
@@ -1,9 +1,9 @@
 start_xterm() {
 	[ -f ~/.Xresources ] && xrdb -l ~/.Xresources
 	if pgrep tmux; then
-		xterm -e tmux attach
+		exec xterm -e tmux attach
 	else
-		xterm -e tmux new-session \; new-window cmus \; select-window -t:^
+		exec xterm -e tmux new-session \; new-window cmus \; select-window -t:^
 	fi
 }
 start_xterm &
